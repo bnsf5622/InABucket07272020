e8a2b63b7f27a21fb316c71c097c3b8b6b28ceb6
chuck francis <chuck_francis10@yahoo.com>
2020-07-27 13:13:30 -0400

    Add project files.

---
+++ InABucket07272020/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace InABucket07272020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // You can declare all KINDS of varibles.
+            String walrus;
+            double piesEaten;
+            float weightOfTeaCupPig;
+            int grainsOfSnd;
+
+            // But declaing them just sets up the spce for data
+            // to use 
+            walrus = "Sir Leroy Jenkins III";
+            piesEaten = 42.1;
+
+            Console.WriteLine("Meet  my pet Walrus, " + walrus);
+            Console.Write("He was a bit hungry today, and ate this many pies : ");
+            Console.WriteLine(piesEaten);
+        }
+    }
+}
